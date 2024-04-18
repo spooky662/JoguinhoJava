b36b1fd2ae25ba140d3e12a0a56a4dbbb2a4d969
Spooky662 <104535161+spooky662@users.noreply.github.com>
2024-04-18 20:53:23 -0300

    Update Main.java
    Acrescentando o movimento do herói.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void mesa(String[][] matrix){
+    public static void mesa(Personagem[][] matrix){
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                if(matrix[i][j] == "X") {
+                if(matrix[i][j].desenho == "X") {
                     System.out.print("[ " + "\u001B[36m" + matrix[i][j] + "\u001B[0m" + " ]");
-                } else if (matrix[i][j] == "0"){
+                } else if (matrix[i][j].desenho == "0"){
                     System.out.print("[ " + "\u001B[35m" + matrix[i][j] + "\u001B[0m" + " ]");
                 } else {
                     System.out.print("[ " + matrix[i][j] + " ]");
@@ -14,11 +16,41 @@ public class Main {
         }
     }
 
+    public static void norte(Personagem[][] matrix, int i, int j){
+        String aux;
+        aux = matrix[i-1][j].desenho;
+        matrix[i-1][j].desenho = matrix[i][j].desenho;
+        matrix[i][j].desenho = aux;
+    }
+
+    public static void sul(Personagem[][] matrix, int i, int j){
+        String aux;
+        aux = matrix[i+1][j].desenho;
+        matrix[i+1][j].desenho = matrix[i][j].desenho;
+        matrix[i][j].desenho = aux;
+    }
+
+    public static void leste(Personagem[][] matrix, int i, int j){
+        String aux;
+        aux = matrix[i][j+1].desenho;
+        matrix[i][j+1].desenho = matrix[i][j].desenho;
+        matrix[i][j].desenho = aux;
+    }
+
+    public static void oeste(Personagem[][] matrix, int i, int j){
+        String aux;
+        aux = matrix[i][j-1].desenho;
+        matrix[i][j-1].desenho = matrix[i][j].desenho;
+        matrix[i][j].desenho = aux;
+
+    }
+
     public static void main(String[] args) {
-        String[][] tabuleiro = new String[10][10];
+        Scanner sc = new Scanner(System.in);
+        Personagem[][] tabuleiro = new Personagem[10][10];
         for (int i = 0; i < 10; i++){
             for (int j = 0; j < 10; j++){
-                tabuleiro[i][j] = ".";
+                tabuleiro[i][j] = new Personagem(i, j, ".");
             }
         }
 
@@ -26,11 +58,51 @@ public class Main {
         System.out.println();
 
         Player hero = new Player(3, 5, "X");
-        tabuleiro[hero.posx][hero.posy] = hero.desenho;
+        tabuleiro[hero.posx][hero.posy] = hero;
 
         Inimigo enemy = new Inimigo(4, 5, "0");
-        tabuleiro[enemy.posx][enemy.posy] = enemy.desenho;
+        tabuleiro[enemy.posx][enemy.posy] = enemy;
 
         mesa(tabuleiro);
+
+        // mover personagem
+        char esc = 's';
+        System.out.println("Mover o personagem?");
+        esc = sc.next().charAt(0);
+        if(esc == 's'){
+            do{
+                System.out.println("Direção: ");
+                System.out.println("1- norte");
+                System.out.println("2- leste");
+                System.out.println("3- sul");
+                System.out.println("4- oeste");
+                int esc2 = sc.nextInt();
+                switch (esc2){
+                    case 1:
+                        norte(tabuleiro, hero.posx, hero.posy);
+                        hero.posx -= 1;
+                        break;
+                    case 2:
+                        leste(tabuleiro, hero.posx, hero.posy);
+                        hero.posy += 1;
+                        break;
+                    case 3:
+                        sul(tabuleiro, hero.posx, hero.posy);
+                        hero.posx += 1;
+                        break;
+                    case 4:
+                        oeste(tabuleiro, hero.posx, hero.posy);
+                        hero.posy -= 1;
+                        break;
+                    default:
+                        break;
+                }
+                mesa(tabuleiro);
+                System.out.println("Mover o personagem?");
+                esc = sc.next().charAt(0);
+            }while(esc == 's');
+        }
+
+        sc.close();
     }
 }
