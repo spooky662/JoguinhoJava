593576625baf6f3cbdbc25fcdf4beff020c2a58c
Spooky662 <104535161+spooky662@users.noreply.github.com>
2024-04-17 21:40:01 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@ public class Main {
     public static void mesa(String[][] matrix){
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                System.out.print("[ " + matrix[i][j] + " ]");
+                if(matrix[i][j] != ".") {
+                    System.out.print("[ " + "\u001B[36m" + matrix[i][j] + "\u001B[0m" + " ]");
+                } else {
+                    System.out.print("[ " + matrix[i][j] + " ]");
+                }
             }
             System.out.println();
         }
@@ -19,8 +23,11 @@ public class Main {
         mesa(tabuleiro);
         System.out.println();
 
-        Personagem personagem1 = new Personagem(3, 5, "<3");
+        Personagem personagem1 = new Personagem(3, 5, "X");
         tabuleiro[personagem1.posx][personagem1.posy] = personagem1.desenho;
+
+        Personagem personagem2 = new Personagem(4, 5, "0");
+        tabuleiro[personagem2.posx][personagem2.posy] = personagem2.desenho;
 
         mesa(tabuleiro);
     }
